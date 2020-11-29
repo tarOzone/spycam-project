57ee1ec356ca3970b85590eafae612bf0db71f98
tarOzone <42716598+tarOzone@users.noreply.github.com>
2020-11-29 12:37:24 +0700

    feat: data are stored by date and time

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import os
 import json
 import pyrebase
 from typing import Union
 from datetime import datetime
-
+from dotenv import load_dotenv
 from data.data_generator import generate_random_image
+
+
+load_dotenv()
 
 
 def _get_datetime_string() -> str:
@@ -23,7 +27,7 @@ def upload_image(storage, content: Union[str, bytes]) -> dict:
     return storage.child(f"/saved-image_{_get_datetime_string()}.jpg").put(content)
 
 
-def upload_data(storage, database, content: Union[str, bytes]):
+def upload_data(storage, database, content: Union[str, bytes], id_token: str):
     metadata = upload_image(storage, content)
     url = storage.child(f"/{metadata['name']}").get_url(metadata['downloadTokens'])
     data = {
@@ -34,23 +38,27 @@ def upload_data(storage, database, content: Union[str, bytes]):
             'longitude': '???',
         },
     }
-    database.push(data)
+    date = datetime.now().strftime("%Y-%m-%d")
+    time = datetime.now().strftime("%H:%M:%S")
+    database.child(date).child(time).push(data, id_token)
+
+
+def retrieve(database):
+    all_users = database.get()
+    for user in all_users.each():
+        print(user.val())
 
 
 def main():
     firebase = pyrebase.initialize_app(_read_firebase_config())
+    auth = firebase.auth()
     storage = firebase.storage()
     database = firebase.database()
 
+    user = auth.sign_in_with_email_and_password(os.getenv('EMAIL'), os.getenv('PASSWORD'))
+
     image = generate_random_image(ext=".png")
-
-    upload_data(storage=storage, database=database, content=image)
-    notify()
-
-    # reterving data using loops
-    # all_users = database.get()
-    # for user in all_users.each():
-    #     print(user.val())
+    upload_data(storage=storage, database=database, content=image, id_token=user["idToken"])
 
 
 if __name__ == '__main__':
