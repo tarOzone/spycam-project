18ad280dea637ced336546736d49b6be4d0d9162
Ugrit Wongkham <6243415-tarOzone@users.noreply.gitlab.com>
2020-12-28 18:41:24 +0700

    refactor: remove not used files

--- app.py
+++ app.py
@@ -1,31 +1,31 @@
 import os
-import RPi.GPIO as GPIO
 from time import time
-from camera import camera
+import RPi.GPIO as GPIO
+from camera2 import CameraCapture2
 from datetime import datetime
 from dotenv import load_dotenv
 
 
 load_dotenv()
 pir_gpio_pin: int = int(os.getenv('PIR_GPIO_PIN'))
+camera: CameraCapture2 = CameraCapture2()
+
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pir_gpio_pin, GPIO.IN)
 
 
-def capture_and_upload():
-    for _ in range(10):
-        filename: str = camera.get_filename(ext='jpg')
-        camera.capture(filename)
-        camera.upload_image(content=filename)
+def capture_and_uploads(n: int):
+    for _ in range(n):
+        start = time()
+        camera.upload()
+        print(f"uploading done in {time() - start}")
 
 
 def callback(channel):
     print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}:", "Intruders found\a")
-    start = time()
-    capture_and_upload()
-    print(f"uploading done in {time() - start}")
+    capture_and_uploads(3)
 
 
 GPIO.add_event_detect(
